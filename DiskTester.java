88dcb6ece0659e12cda68167da1206b27889c0a0
Philip Rodriguez <philiprodriguez@knights.ucf.edu>
2019-07-28 15:12:42 -0400

    Altered much of the code so that it supports writing to more than one single file now, supports rw to a directory other than the program's working directory, and reads and writes much faster so larger files can be used (e.g. you want to be overfilling the cache of the disk in question)

--- DiskTester.java
+++ DiskTester.java
@@ -10,98 +10,218 @@ import java.util.*;
 import java.io.*;
 
 public class DiskTester {
-
-    private static final Scanner scan = new Scanner(System.in);
+    // Arguments to be filled in before starting
+    private static int fileCount;
     private static int fileSize;
-    private static byte[] fileInMemory;
-    private static RandomAccessFile raf;
-    
-    private static int totalReadFailures = 0;
-    private static int totalWriteFailures = 0;
-    private static int totalOtherFailures = 0;
-    private static int totalWrites = 0;
-    private static int totalReads = 0;
+    private static String dirPath = "./";
 
-    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+    // Other stuff
+    private static final Scanner scan = new Scanner(System.in);
+
+    private static byte[][] filesInMemory;
+    private static RandomAccessFile[] randomAccessFiles;
+
+    private static long totalReadFailures = 0;
+    private static long totalWriteFailures = 0;
+    private static long totalOtherFailures = 0;
+    private static long totalWrites = 0;
+    private static long totalReads = 0;
+
+    public static void main(String[] args) throws InterruptedException {
         prepareArguments(args);
-        
+
+        System.out.print("Allocating memory...");
+        filesInMemory = new byte[fileCount][fileSize];
+        System.out.println("OK");
+
         Random r = new Random();
-        raf = new RandomAccessFile("DiskTester.dat", "rws");
-        while (true) {
-            fileInMemory = new byte[fileSize];
-            r.nextBytes(fileInMemory); 
-            // Write
-            try {
-                raf.seek(0);
-            } catch (IOException exc) {
-                System.out.println("Failed to seek to position 0!");
-                totalOtherFailures++;
-                exc.printStackTrace();
-                continue;
+
+        int iteration = 1;
+        outer: while (true) {
+            printSummary();
+            Thread.sleep(5000);
+            //TODO: cleanup previous iteration here
+
+            System.out.println("Iteration " + iteration++);
+
+            System.out.print("Determining file content...");
+            for (int i = 0; i < fileCount; i++) {
+                r.nextBytes(filesInMemory[i]);
             }
-            
-            long start = System.currentTimeMillis();
-            System.out.println("Writing bytes to file...");
-            for (int i = 0; i < fileSize; i++) {
-                totalWrites++;
+            System.out.println("OK");
+
+            System.out.print("Creating files for writing...");
+            randomAccessFiles = new RandomAccessFile[fileCount];
+            for (int i = 0; i < fileCount; i++) {
                 try {
-                    raf.write(fileInMemory[i]);
+                    randomAccessFiles[i] = new RandomAccessFile(dirPath + "DiskTester" + (i + 1) + ".dat", "rws");
+                } catch (FileNotFoundException exc) {
+                    System.out.println("(!!!) FAILED to create file " + (i+1) + "!");
+                    totalOtherFailures++;
+                    exc.printStackTrace();
+                    continue outer;
+                }
+            }
+            System.out.println("OK");
+
+            System.out.println("Writing file content...");
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < fileCount; i++) {
+                try {
+                    randomAccessFiles[i].seek(0);
                 } catch (IOException exc) {
+                    System.out.println("(!!!) FAILED to seek to position 0 in file " + (i+1) + "!");
+                    totalOtherFailures++;
+                    exc.printStackTrace();
+                    continue outer;
+                }
+
+                totalWrites+=1;
+                try {
+                    randomAccessFiles[i].write(filesInMemory[i]);
+                    randomAccessFiles[i].getFD().sync();
+                    randomAccessFiles[i].close();
+                } catch (IOException exc) {
+                    System.out.println("(!!!) FAILED to write to file " + (i+1) + "!");
                     totalWriteFailures++;
                     exc.printStackTrace();
+                    continue outer;
                 }
             }
             long end = System.currentTimeMillis();
-            System.out.println("Writing finished, took " + (end-start) + "ms");
-            
-            // Read
-            try {
-                raf.seek(0);
-            } catch (IOException exc) {
-                System.out.println("Failed to seek to position 0!");
-                totalOtherFailures++;
-                exc.printStackTrace();
-                continue;
-            }
-            start = System.currentTimeMillis();
-            System.out.println("Reading all indices...");
-            for (int i = 0; i < fileSize; i++) {
-                totalReads++;
+            System.out.println("OK");
+            System.out.println("Writing took " + (end-start) + "ms");
+
+            System.out.println("Re-opening files for reading...");
+            randomAccessFiles = new RandomAccessFile[fileCount];
+            for (int i = 0; i < fileCount; i++) {
                 try {
-                    byte read = raf.readByte();
-                    if (fileInMemory[i] != read) {
-                        System.out.println("FAILED! Should have been " + fileInMemory[i]);
-                        Thread.sleep(1000);
-                    } else {
+                    randomAccessFiles[i] = new RandomAccessFile(dirPath + "DiskTester" + (i + 1) + ".dat", "r");
+                } catch (FileNotFoundException exc) {
+                    System.out.println("(!!!) FAILED to open file " + (i+1) + "!");
+                    totalOtherFailures++;
+                    exc.printStackTrace();
+                    continue outer;
+                }
+            }
+            System.out.println("OK");
+
+            // Read
+            System.out.println("Reading file content...");
+            start = System.currentTimeMillis();
+            for (int i = 0; i < fileCount; i++) {
+                try {
+                    randomAccessFiles[i].seek(0);
+                } catch (IOException exc) {
+                    System.out.println("(!!!) FAILED to seek to position 0 in file " + (i+1) + "!");
+                    totalOtherFailures++;
+                    exc.printStackTrace();
+                    continue;
+                }
+
+                byte[] readBuffer = new byte[4096];
+                int bytesRead = -1;
+                int placeInFile = 0;
+                try {
+                    while ((bytesRead = randomAccessFiles[i].read(readBuffer)) != -1) {
+                        totalReads += bytesRead;
+
+                        // Examine the bytes!
+                        for (int b = 0; b < bytesRead; b++) {
+                            if (filesInMemory[i][placeInFile] != readBuffer[b]) {
+                                System.out.println("(!!!) FAILED! Should have been " + filesInMemory[i][placeInFile] + " but got " + readBuffer[b] + " instead!");
+                                totalReadFailures++;
+                                Thread.sleep(1000);
+                            }
+                            placeInFile++;
+                        }
                     }
                 } catch (IOException exc) {
+                    System.out.println("(!!!) FAILED! Could not read byte!");
                     totalReadFailures++;
-                    System.out.println("FAILED! Could not read!");
                     exc.printStackTrace();
+                }
+
+                // End of file reached but was that correct?
+                if (placeInFile < fileSize) {
+                    System.out.println("(!!!) FAILED! Premature end of file at " + placeInFile + " when it should have been at " + fileSize);
+                    totalReadFailures++;
+                    Thread.sleep(1000);
                 }
             }
             end = System.currentTimeMillis();
-            System.out.println("Reading finished, took " + (end-start) + "ms");
-            System.out.println("Total Reads = " + totalReads);
-            System.out.println("Total Read Failures = " + totalReadFailures);
-            System.out.println("Total Writes = " + totalWrites);
-            System.out.println("Total Write Failures = " + totalWriteFailures);
-            System.out.println("Total Other Failures = " + totalOtherFailures);
-            
-            Thread.sleep(5000);
+            System.out.println("OK");
+            System.out.println("Reading took " + (end-start) + "ms");
         }
     }
-    
-    public static void prepareArguments(String[] args) {
+
+    private static void printSummary() {
+        System.out.println("--------------------Summary--------------------");
+        System.out.println("Total Reads = " + totalReads);
+        System.out.println("Total Read Failures = " + totalReadFailures);
+        System.out.println("Total Writes = " + totalWrites);
+        System.out.println("Total Write Failures = " + totalWriteFailures);
+        System.out.println("Total Other Failures = " + totalOtherFailures);
+    }
+
+    private static boolean isPositiveIntegerString(String s) {
+        return s.matches("[0-9]+") && s.charAt(0) != '0';
+    }
+
+    private static void printUsage() {
+        System.out.println("USAGE:");
+        System.out.println("java DiskTester -fileSize (size in bytes) -fileCount (count integer) [-dirPath (path to working directory)]");
+    }
+
+    private static void prepareArguments(String[] args) {
         if (args.length > 0) {
-            if (args[0].matches("[0-9]+")) {
-                fileSize = Integer.parseInt(args[0]);
-            } else {
-                System.out.println("File size must be a positive integer!");
+            boolean fileCountSatisfied = false;
+            boolean fileSizeSatisfied = false;
+
+            for (int a = 0; a < args.length; a++) {
+                if (args[a].equals("-fileSize")) {
+                    if (isPositiveIntegerString(args[a+1])) {
+                        fileSize = Integer.parseInt(args[a+1]);
+                        a++;
+                        fileSizeSatisfied = true;
+                    } else {
+                        System.out.println("File size must be a positive integer.");
+                    }
+                } else if (args[a].equals("-fileCount")) {
+                    if (isPositiveIntegerString(args[a+1])) {
+                        fileCount = Integer.parseInt(args[a+1]);
+                        a++;
+                        fileCountSatisfied = true;
+                    } else {
+                        System.out.println("File count must be a positive integer.");
+                    }
+                } else if (args[a].equals("-dirPath")) {
+                    dirPath = args[a+1];
+                    a++;
+                    if (!dirPath.endsWith("/"))
+                        dirPath += "/";
+                } else {
+                    System.out.println("Illegal argument at position " + a + ": " + args[0]);
+                    printUsage();
+                    System.exit(1);
+                }
+            }
+
+            if (!(fileCountSatisfied && fileSizeSatisfied)) {
+                System.out.println("One or more required arguments unsatisfied.");
+                printUsage();
+                System.exit(1);
             }
         } else {
+            printUsage();
+
             System.out.println("Enter the desired file size to test with:");
             fileSize = scan.nextInt();
+            System.out.println("Enter the desired file count to test with:");
+            fileCount = scan.nextInt();
+            System.out.println("Enter the desired working directory:");
+            scan.nextLine();
+            dirPath = scan.nextLine();
         }
     }
 }
